966e685d837e7d37496b857bfa2d324559001e55
Manya Sharma <manya.sharma395@gmail.com>
2026-06-12 20:47:48 +0530

    Basic layout of home page

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -57,7 +57,7 @@ const AuthForm = ({type} : {type: FormType}) => {
         <div className="card-border lg:min-w-[566px]">
             <div className="flex flex-col gap-6 card py-14 px-10">
                 <div className="flex flex-row gap-2 justify-center">
-                    <Image src="public/logo.svg" alt="logo" height={32} width={38} />
+                    <Image src="/logo.svg" alt="logo" height={32} width={38} />
                     <h2 className="text-primary-100">MockMate</h2>
                 </div>
 
